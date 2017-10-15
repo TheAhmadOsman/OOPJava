970253d1561b1d19f3ca00fad07bb6d637922d4d
Roman Yasinovskyy <yasiro01@luther.edu>
2017-10-15 14:06:56 -0500

    Exercise 1 template and output

---
+++ src/exercise1/Ex1Driver.java
@@ -0,0 +1,95 @@
+package exercise1;
+
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * @author yasiro01
+ */
+public class Ex1Driver {
+  public static void main(String args[]) {
+    Random rnd = new Random();
+    rnd.setSeed(252);
+    int[] numbers = new int[10];
+    for (int i = 0; i < 10; i++) {
+      numbers[i] = rnd.nextInt(10);
+    }
+    task1();
+    task2();
+    task3();
+    task4();
+    task5();
+    task6();
+    task7();
+    task8(numbers);
+    task9();
+    task10();
+  }
+
+  /**
+   * TODO: Task 1: Print "Hello World" to the default system output
+   */
+  public static void task1() {
+    throw new UnsupportedOperationException("Task 1 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 2: Print all numbers between 1 and 100 (inclusive) replacing all multiples of 3 with "foo", multiples of 5 with "bar", multiples of 3 and 5 with "huh"
+   */
+  public static void task2() {
+    throw new UnsupportedOperationException("Task 2 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 3: Calculate and print the sum of all multiples of 7 between 1 and 100 (inclusive)
+   */
+  public static void task3() {
+    throw new UnsupportedOperationException("Task 3 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 4: Calculate and print sum and product of all numbers between 1 and 20 (inclusive)
+   */
+  public static void task4() {
+    throw new UnsupportedOperationException("Task 4 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 5: Ask a user to enter their full name and print the initials (ie John Doe should yield JD)
+   */
+  public static void task5() {
+    throw new UnsupportedOperationException("Task 5 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 6: Create a static array of 10 elements and initiate each element in it to the cube of its index (Do it in a loop, not during declaration)
+   */
+  public static void task6() {
+    throw new UnsupportedOperationException("Task 6 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 7: Create an array of 100 random integers between 1 and 100, ask the user for an integer and tell if that number is in the array
+   */
+  public static void task7() {
+    Random rnd = new Random();
+    rnd.setSeed(252);
+    throw new UnsupportedOperationException("Task 7 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 8: Write a function that takes an array of integers as a parameter and returns the sum of its elements
+   * @param numbers
+   * @return 
+   */
+  public static int task8(int[] numbers) {
+    throw new UnsupportedOperationException("Task 8 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 9: Ask a user to enter a word and write a function that tells if letters of a word are ordered (ie "buy" would return true, while "bye" - false)
+   * @return 
+   */
+  public static boolean task9() {
+    throw new UnsupportedOperationException("Task 9 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 10: Implement sieve of Eratosthenes and print all prime numbers between 1 and 100
+   */
+  public static void task10() {
+    throw new UnsupportedOperationException("Task 3 is not implemented yet.");
+  }
+
+}
