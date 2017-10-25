98ffe11f2029a354c6761afdd03f75bf15ffdf17
Roman Yasinovskyy <yasiro01@luther.edu>
2017-10-25 10:00:29 -0500

    Exercise 2 template

---
+++ src/exercise2/Ex2Driver.java
@@ -0,0 +1,109 @@
+package exercise2;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author yasiro01
+ */
+public class Ex2Driver {
+  public static void main(String args[]) {
+    Random rnd = new Random();
+    rnd.setSeed(252);
+    int[] numberArr = new int[10];
+    for (int i = 0; i < 10; i++) {
+      numberArr[i] = rnd.nextInt(100);
+    }
+    ArrayList<Integer> numberLst = new ArrayList();
+    for (int i = 0; i < 10; i++) {
+      numberLst.add(rnd.nextInt(100));
+    }
+    task1(3);
+    System.out.println(task2(numberArr, 90));
+    System.out.println(task3(numberArr, 75));
+    task4(numberArr);
+    task5(numberArr);
+    task6();
+    task7();
+    System.out.println(task8(numberLst, 75));
+    task9();
+    task10(task9());
+  }
+
+  /**
+   * TODO: Task 1: Define and call a function nHeads() that takes a number (n) as a parameter and prints the result of coin flips ("Heads" or "Tails" with equal probability) until there are n "Heads" in a row. Seed the randomizer in the main() function.
+   * @param n
+   */
+  public static void task1(int n) {
+    throw new UnsupportedOperationException("Task 1 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 2: Define a function that takes an array of integers and a number as parameters and returns boolean True if the number provided is larger than every element of the array, False otherwise.
+   * @param arr
+   * @param n
+   * @return 
+   */
+  public static boolean task2(int[] arr, int n) {
+    throw new UnsupportedOperationException("Task 2 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 3: Write a function that takes an array of integers and a threshold and prints how many values are below the threshold.
+   * @param arr
+   * @param threshold
+   * @return 
+   */
+  public static int task3(int[] arr, int threshold) {
+    throw new UnsupportedOperationException("Task 3 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 4: Write a function that takes an array of integers as a parameter, finds the difference between 100 and the largest element of the array and adds this difference to every element in the array. (Note: after this function call the largest value in the array must be 100.)
+   * @param arr
+   */
+  public static void task4(int[] arr) {
+    throw new UnsupportedOperationException("Task 4 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 5: Write a function that takes an array of integers as a parameter and, assuming the values are scores, prints number of students with 'A', 'B', 'C', 'D' and 'F'.
+   * @param arr
+   */
+  public static void task5(int[] arr) {
+    throw new UnsupportedOperationException("Task 5 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 6: Create an ArrayList of double values and ask a user to start entering numbers. Keep asking and adding numbers to the ArrayList until the user enters 0, then stop and print all the values in the ArrayList. Use function add() to add a number to an ArrayList.
+   */
+  public static void task6() {
+    ArrayList<Double> arr = new ArrayList();
+    throw new UnsupportedOperationException("Task 6 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 7: Create an ArrayList of integer values of size 10 and initialize each element of the ArrayList to a random number between 50 and 100.
+   */
+  public static void task7() {
+    throw new UnsupportedOperationException("Task 7 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 8: Define a function that takes an ArrayList of integer values and a threshold as parameters and returns the number of values in the ArrayList below a threshold. You may use the ArrayList created at the previous step.
+   * @param arrLst
+   * @param threshold
+   * @return 
+   */
+  public static int task8(ArrayList<Integer> arrLst, int threshold) {
+    throw new UnsupportedOperationException("Task 8 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 9: Generate a 2-dimensional array of size 10x10. Initialize values in the array to the values in the multiplication table of size 10. (e.g. values in the intersections must start with 1 (1x1) and go all the way up to 100). Print the array and return it.
+   * @return 
+   */
+  public static int[][] task9() {
+    throw new UnsupportedOperationException("Task 9 is not implemented yet.");
+  }
+  /**
+   * TODO: Task 10: Calculate and print the sum of values in each row and each column of the matrix generated at the previous step.
+   * @param matrix
+   */
+  public static void task10(int[][] matrix) {
+    throw new UnsupportedOperationException("Task 3 is not implemented yet.");
+  }
+
+}
