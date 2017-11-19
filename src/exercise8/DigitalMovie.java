c02172a62a1297012f38ba02c1a38fac6cfae194
Ahmad M. Osman <osmaah02@luther.edu>
2017-11-18 23:52:02 -0600

    Exercise 8 Submission

--- src/exercise8/DigitalMovie.java
+++ src/exercise8/DigitalMovie.java
@@ -3,13 +3,38 @@ package exercise8;
 import java.util.ArrayList;
 
 /**
+ *
  * Class DigitalMovie
  * MediaLIbrary code 2
- * @author yasiro01
- */
+ * Implement the constructor that calls a constructor of the superclass
+ * Override methods checkin and checkout. They should do nothing
+ * Implement method getInfo that returns detailed description of a digital movie
+ * @author Ahmad M. Osman - Original template by Dr. Roman Yasinovskyy
+ * @assignment Week 5: Exercise 8
+ * 
+**/
+
 public class DigitalMovie extends Movie {
   private String service;
+  
+    public DigitalMovie(String service, String director, int releaseYear, long id, String title) {
+        super(director, releaseYear, id, title, 1);
+        this.service = service;
+    }
 
+    @Override
+    public void checkin() {
+    }
+
+    @Override
+    public void checkout() {
+    }
+
+    @Override
+    public String getInfo() {
+        return title + " (" + releaseYear + ")" + " by " + director + " is available via " + service;
+    }
+    
   /**
    * Get the value of service
    * @return the value of service
--- src/exercise8/EBook.java
+++ src/exercise8/EBook.java
@@ -1,12 +1,37 @@
 package exercise8;
 
 /**
+ * 
  * Class EBook
  * MediaLIbrary code 4
- * @author yasiro01
- */
+ * Implement the constructor that calls a constructor of the superclass
+ * Override methods checkin and checkout. They should do nothing
+ * Implement method getInfo that returns detailed description of an ebook
+ * @author Ahmad M. Osman - Original template by Dr. Roman Yasinovskyy
+ * @assignment Week 5: Exercise 8
+ * 
+**/
+
 public class EBook extends Book {
   private final String format;
+
+    public EBook(String format, String author, int publishingYear, long id, String title) {
+        super(author, publishingYear, id, title, 1);
+        this.format = format;
+    }
+
+    @Override
+    public void checkin() {
+    }
+
+    @Override
+    public void checkout() {
+    }
+
+    @Override
+    public String getInfo() {
+        return author + " (" + publishingYear + "). " + title + " is available for " + format;
+    }
   
   /**
    * Get the book format
